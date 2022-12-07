61fed54ab0ccd293a8f00ac9a1cec560a8ac31dc
anastazy06 <kononenko2021ek11@student.karazin.ua>
2022-12-07 23:39:30 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './index.css'
+import Modal from './Modal'
+
+function App() {
+    const [open, setOpen] = React.useState(false)
+
+    return (
+        <div className='App'>
+            <button onClick={() => setOpen(true)} className='open-modal-btn'>
+                Відкрити вікно
+            </button>
+            <Modal open={open} setOpen={setOpen} />
+        </div>
+    )
+}
+
+export default App
